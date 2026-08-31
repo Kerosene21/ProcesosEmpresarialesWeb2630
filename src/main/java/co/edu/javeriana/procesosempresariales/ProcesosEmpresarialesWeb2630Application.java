16b8a4e6cbfb1066ff2a91e2d1b3ee148a8d7598
David Orjuela <davido.orjuela@javeriana.edu.co>
2026-08-31 13:58:58 -0500

    feat: inicializar proyecto Spring Boot

---
+++ src/main/java/co/edu/javeriana/procesosempresariales/ProcesosEmpresarialesWeb2630Application.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.procesosempresariales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcesosEmpresarialesWeb2630Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcesosEmpresarialesWeb2630Application.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/javeriana/procesosempresariales/ProcesosEmpresarialesWeb2630ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.procesosempresariales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcesosEmpresarialesWeb2630ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
